1aba26361f122d359dcb37c0daf5f727db44094e
PedroBelvedere <112791924+PedroBelvedere@users.noreply.github.com>
2022-12-25 20:15:22 -0300

    redes sociales

--- portfoliopedro/src/app/app.module.ts
+++ portfoliopedro/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { RedesComponent } from './components/redes/redes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LogoAPComponent
+    LogoAPComponent,
+    RedesComponent
   ],
   imports: [
     BrowserModule
