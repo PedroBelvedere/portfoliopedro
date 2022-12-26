c003e795aa5e3eff061d8e91e6c63e46a433c8ad
PedroBelvedere <112791924+PedroBelvedere@users.noreply.github.com>
2022-12-26 20:54:55 -0300

    Acerca de

--- portfoliopedro/src/app/app.module.ts
+++ portfoliopedro/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
 import { RedesComponent } from './components/redes/redes.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     LogoAPComponent,
-    RedesComponent
+    RedesComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule
