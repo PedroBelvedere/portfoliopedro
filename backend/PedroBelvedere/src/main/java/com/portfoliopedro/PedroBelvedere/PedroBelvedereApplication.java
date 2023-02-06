46be8ad1e498ed7e67fde95ddcaffc08dde44281
PedroBelvedere <Pedrorobel3@gmail.com>
2023-02-06 17:43:33 -0300

    Agregado el backend

---
+++ backend/PedroBelvedere/src/main/java/com/portfoliopedro/PedroBelvedere/PedroBelvedereApplication.java
@@ -0,0 +1,13 @@
+package com.portfoliopedro.PedroBelvedere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedroBelvedereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedroBelvedereApplication.class, args);
+	}
+
+}
---
+++ backend/PedroBelvedere/src/test/java/com/portfoliopedro/PedroBelvedere/PedroBelvedereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfoliopedro.PedroBelvedere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedroBelvedereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
