fac479447ccb0fa2fce5e4f1f3084c33adfc407b
PedroBelvedere <Pedrorobel3@gmail.com>
2023-03-05 13:07:48 -0300

    Compleción del javapackage Security

---
+++ backend/PedroBelvedere/src/main/java/com/portfoliopedro/PedroBelvedere/seguridad/DTO/JwtDTO.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.portfoliopedro.PedroBelvedere.seguridad.DTO;
+
+import java.util.Collection;
+import org.springframework.security.core.GrantedAuthority;
+
+/**
+ *
+ * @author pedro
+ */
+public class JwtDTO {
+    private String token;
+    private String bearer = "Bearer";
+    private String nombreUsuario;
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public JwtDTO(String token, String nombreUsuario, Collection<? extends GrantedAuthority> authorities) {
+        this.token = token;
+        this.nombreUsuario = nombreUsuario;
+        this.authorities = authorities;
+    }
+
+    public JwtDTO(String jwt, String username, Collection<? extends GrantedAuthority> authorities, String password) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getBearer() {
+        return bearer;
+    }
+
+    public void setBearer(String bearer) {
+        this.bearer = bearer;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    public void setAuthorities(Collection<? extends GrantedAuthority> authorities) {
+        this.authorities = authorities;
+    }
+    
+    
+    
+}
--- backend/PedroBelvedere/src/main/java/com/portfoliopedro/PedroBelvedere/seguridad/controller/AuthController.java
+++ backend/PedroBelvedere/src/main/java/com/portfoliopedro/PedroBelvedere/seguridad/controller/AuthController.java
@@ -4,6 +4,9 @@
  */
 package com.portfoliopedro.PedroBelvedere.seguridad.controller;
 
+import com.portfoliopedro.PedroBelvedere.seguridad.DTO.JwtDTO;
+import com.portfoliopedro.PedroBelvedere.seguridad.DTO.LoginUsuario;
+import com.portfoliopedro.PedroBelvedere.seguridad.DTO.NuevoUsuario;
 import com.portfoliopedro.PedroBelvedere.seguridad.Entity.Rol;
 import com.portfoliopedro.PedroBelvedere.seguridad.Entity.User;
 import com.portfoliopedro.PedroBelvedere.seguridad.Enums.RolNombre;
@@ -17,9 +20,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -49,10 +57,10 @@ public class AuthController {
     if(bindingResult.hasErrors())
         return new ResponseEntity(new Mensaje("Información mal introducida o inválida."), HttpStatus.BAD_REQUEST);
     
-    if(userServicio.existByNombreUsuario(nombreUsuario.getNombreUsuario()))
+    if(userServicio.existByNombreUsuario(nuevoUsuario.getNombreUsuario()))
         return new ResponseEntity(new Mensaje("Usuario ya existente."), HttpStatus.BAD_REQUEST);
     
-    if(userServicio.existByEmail(nombreUsuario.getEmail()))
+    if(userServicio.existByEmail(nuevoUsuario.getEmail()))
         return new ResponseEntity(new Mensaje("Email ya en uso."), HttpStatus.BAD_REQUEST);
     
     User user = new User(nuevoUsuario.getNombre(), nuevoUsuario.getNombreUsuario(), nuevoUsuario.getEmail(), passwordEncoder.encode(nuevoUsuario.getPassword()));
@@ -61,7 +69,7 @@ public class AuthController {
     roles.add(rolServicio.getByRolNombre(RolNombre.ROLE_USER).get());
     
     if(nuevoUsuario.getRoles().contains("admin"))
-        roles.add(rolService.getByRolNombre(RolNombre.ROLE_ADMIN).get());
+        roles.add(rolServicio.getByRolNombre(RolNombre.ROLE_ADMIN).get());
     
     user.setRoles(roles);
     userServicio.save(user);
@@ -69,4 +77,26 @@ public class AuthController {
     return new ResponseEntity(new Mensaje("Usuario creado exitosamente."), HttpStatus.CREATED);
     }
     
+    
+    @PostMapping("/login")
+    public ResponseEntity<JwtDTO>login(@Valid @RequestBody LoginUsuario loginUsuario, BindingResult bindingResult){
+    if(bindingResult.hasErrors())
+        return new ResponseEntity(new Mensaje("ERROR."), HttpStatus.BAD_REQUEST);
+    
+    Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginUsuario.getNombreUsuario(), loginUsuario.getPassword()));
+    
+    
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        
+        String jwt = jwtProvider.generateToken(authentication);
+    
+    
+    UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+    
+    
+    
+    JwtDTO jwtDTO = new JwtDTO(jwt, userDetails.getUsername(), userDetails.getAuthorities(), userDetails.getPassword());
+    
+    return new ResponseEntity(jwtDTO, HttpStatus.OK);
+    }
 }
